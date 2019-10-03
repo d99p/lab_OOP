07968ed3a3daed2330d851c5affb71acf63a1d01
Perepelytsia Denys <32012489+d99p@users.noreply.github.com>
2019-10-03 14:34:58 +0300

    Add files via upload

---
+++ Hotel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace Lab1_OOP
+{
+    class Hotel
+    {
+    public string name;
+    public  int floorNum;
+    public int roomNum;
+    public int maxVisitors;
+    public int currVisitors;
+    public bool isNoSmoking;
+
+       public void HotelStat() // Hotel information
+        {
+            Console.WriteLine("Стан готелю: ");
+            Console.WriteLine("Ім'я готелю: " + name);
+            Console.WriteLine("Кількість поверхів: " + floorNum);
+            Console.WriteLine("Кількість кімнат " + roomNum);
+            Console.WriteLine("Максимальна кількість гостей " + maxVisitors);
+            Console.WriteLine("Поточна кількість гостей " + currVisitors);
+            Console.WriteLine("Дозволено куріння в готелі " + isNoSmoking);
+        }
+
+        public void AddVisitors(int num) //Add visitors
+        {
+           { 
+            if (num >= 1)
+                currVisitors = (currVisitors + num);
+            else
+                Console.WriteLine("Невірний запис");
+            }
+        }
+        public void ReduceVisitors(int outVisitors) // ReduceVisitors 
+        {
+            currVisitors = (currVisitors - outVisitors);
+        }
+
+        public void currInfo() // Info hotel Visitors
+        {
+            Console.WriteLine("Кількість постоляців на даний момент: " + currVisitors);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Lab1_OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Hotel h1 = new Hotel();
+            h1.name = "Dolphin";
+            h1.floorNum = 6;
+            h1.roomNum = 15;
+            h1.maxVisitors = 120;
+            h1.currVisitors = 64;
+            h1.isNoSmoking = false;
+            h1.HotelStat();
+            h1.AddVisitors(8);
+            h1.ReduceVisitors(6);
+            h1.currInfo();
+            Console.ReadKey();
+        }
+    }
+}
